512ca6978bdc67ba15b0e3784786887260cb3c16
IvO <yves.chemisky@gmail.com>
2015-09-25 11:09:58 -0400

    Changed the global environment variables for more coherency

--- include/smartplus/parameter.hpp
+++ include/smartplus/parameter.hpp
@@ -4,28 +4,8 @@
 ///@version 1.0
 ///@date 01/27/2014
 
-#ifndef maxitNewton
-#define maxitNewton 100
-#endif
-
-#ifndef precision
-#define precision 1E-9
-#endif
-
-#ifndef limit
-#define limit 1E-9
-#endif
-
-#ifndef iota
-#define iota 1E-12
-#endif
-
-#ifndef pi
-#define pi 3.14159265358979323846
-#endif
-
-#ifndef	mini_tnew_dt
-#define mini_tnew_dt 1.E-6
+#ifndef version_full
+#define version_full 1
 #endif
 
 #ifndef axis_psi
@@ -40,18 +20,51 @@
 #define axis_phi 3
 #endif
 
-#ifndef maxiterStruct
-#define maxiterStruct 100
+#ifndef limit
+#define limit 1E-9
 #endif
 
-#ifndef iotaStruct
-#define iotaStruct 1E-6
+#ifndef iota
+#define iota 1E-12
 #endif
 
-#ifndef maxiterMicro
-#define maxiterMicro 100
+#ifndef pi
+#define pi 3.14159265358979323846
 #endif
 
-#ifndef iotaMicro
-#define iotaMicro 1E-6
+#ifndef miniter_umat
+#define miniter_umat 10
+#endif
+
+#ifndef maxiter_umat
+#define maxiter_umat 100
+#endif
+
+#ifndef precision_umat
+#define precision_umat 1E-9
+#endif
+
+
+#ifndef miniter_solver
+#define miniter_solver 10
+#endif
+
+#ifndef maxiter_solver
+#define maxiter_solver 100
+#endif
+
+#ifndef precision_solver
+#define precision_solver 1E-6
+#endif
+
+#ifndef inforce_solver
+#define inforce_solver 1
+#endif
+
+#ifndef maxiter_micro
+#define maxiter_micro 100
+#endif
+
+#ifndef precision_micro
+#define precision_micro 1E-6
 #endif
