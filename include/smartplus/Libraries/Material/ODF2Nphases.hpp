fda764fe0a9c008dee5235e7997ce5717554fffb
IvO <yves.chemisky@gmail.com>
2015-12-05 23:58:37 -0500

    removed pi as an unused constant *0.

--- include/smartplus/Libraries/Material/ODF2Nphases.hpp
+++ include/smartplus/Libraries/Material/ODF2Nphases.hpp
@@ -28,9 +28,9 @@
 namespace smart{
 
 //This function computes the ODF of the selected angle, according to different methods (Lorentzian, Pearson...)
-double ODF(const double&, const int&, const arma::vec&, const bool&, const double& = 0.*pi);
+double ODF(const double&, const int&, const arma::vec&, const bool&, const double& = 0.);
 
 //Writes the Nphases.dat file for multiphase modeling, according to specific ODFs
-    void ODF2Nphases(const arma::Col<int> &, const arma::Col<int> &, const arma::Col<int> &, const std::vector<std::string> &, const arma::mat &, const bool& = false, const double& = 0.*pi);
+    void ODF2Nphases(const arma::Col<int> &, const arma::Col<int> &, const arma::Col<int> &, const std::vector<std::string> &, const arma::mat &, const bool& = false, const double& = 0.);
 
 } //namespace smart
