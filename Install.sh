b582da4a95034b95c8fd94cb52779a4fd5cfccdb
kbonnay <kevin.bonnay@gmail.com>
2016-02-05 15:33:12 +0100

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -7,16 +7,16 @@ current_dir=$(pwd)
 if [ ! -d "build" ]
 then
 	mkdir ${current_dir}/build
-	echo "Folder created \n"
+	echo "Folder created\n"
 else
 	echo "Build directory already exists"
 	
 	while true; do
-		read -p "Do you want to erase old compilation files ? \n y for yes or n for no (recommended)" yn
+		read -p "Do you want to erase old comilation files ? " yn
 		case $yn in
 			[YyOo]* ) rm -r ${current_dir}/build/*; break;;
 			[Nn]* ) break;;
-			* ) echo "Please answer yes (y) or no (n).";;
+			* ) echo "Please answer yes or no.";;
 		esac
 	done
 fi
@@ -27,23 +27,28 @@ cmake ..
 echo ""
 make 
 
-echo "\n---------------------------"
+if [ $? -eq 0 ]
+then
+	echo "\n---------------------------"
 
-if [ -f ${current_dir}/build/CMakeFiles/umat.dir/software/umat_single.cpp.o ]
-then 
-	cp ${current_dir}/build/CMakeFiles/umat.dir/software/umat_single.cpp.o ${current_dir}/build/bin/umat_single.o
-	echo "umat_single.o copied in ${current_dir}/build/bin"
+	if [ -f ${current_dir}/build/CMakeFiles/umat.dir/software/umat_single.cpp.o ]
+	then 
+		cp ${current_dir}/build/CMakeFiles/umat.dir/software/umat_single.cpp.o ${current_dir}/build/bin/umat_single.o
+		echo "umat_single.o copied in ${current_dir}/build/bin"
+	fi
+
+	if [ -f ${current_dir}/build/CMakeFiles/umatT.dir/software/umat_singleT.cpp.o ]
+	then 
+		cp ${current_dir}/build/CMakeFiles/umatT.dir/software/umat_singleT.cpp.o ${current_dir}/build/bin/umat_singleT.o
+		echo "umat_singleT.o copied in ${current_dir}/build/bin"
+	fi
+
+	cp ${current_dir}/build/bin/solver ${current_dir}/exec
+	echo "Solver copied in ${current_dir}/exec"
+	echo "libsmartplus.so available in ${current_dir}/lib"
+	echo "---------------------------"
+	echo "Compilation of SMART+ done.\n"
+else
+	echo "\n---------------------------"
+	echo "Compilation of SMART+ failed.\n"
 fi
-
-if [ -f ${current_dir}/build/CMakeFiles/umatT.dir/software/umat_singleT.cpp.o ]
-then 
-	cp ${current_dir}/build/CMakeFiles/umatT.dir/software/umat_singleT.cpp.o ${current_dir}/build/bin/umat_singleT.o
-	echo "umat_singleT.o copied in ${current_dir}/build/bin"
-fi
-
-cp ${current_dir}/build/bin/solver ${current_dir}/exec
-echo "Solver copied in ${current_dir}/exec"
-echo "libsmartplus.so available in ${current_dir}/lib"
-echo "---------------------------"
-echo "Compilation of SMART+ done. \n"
-
