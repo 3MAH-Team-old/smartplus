a6eeee771d5c9cc5b70aafa7a918e804fa8a361a
chemisky <yves.chemisky@gmail.com>
2016-05-20 22:37:40 -0400

    Ladeveze Le Dantec damage model

---
+++ include/smartplus/Umat/Mechanical/Damage/damage_LLD_0.hpp
@@ -0,0 +1,26 @@
+/* This file is part of SMART+ private.
+ 
+ Only part of SMART+ is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This file is not be distributed under the terms of the GNU GPL 3.
+ It is a proprietary file, copyrighted by the authors
+ */
+
+///@file damage_LLD_m.hpp
+///@brief Modified Ladeveze-Le Dantec model that accounts for uniaxial damage and anisotopic plasticity
+///@brief Damage evolution is considered as a function of time
+///@author Y. Chemisky
+
+#pragma once
+
+#include <iostream>
+#include <armadillo>
+
+namespace smart {
+    
+    void umat_damage_LLD_0(const arma::vec &, const arma::vec &, arma::vec &, arma::mat &, const arma::mat &, const int &, const arma::vec &, const int &, arma::vec &, const double &, const double &,const double &,const double &, double &, double &, const int &, const int &, const bool &, double &);
+    
+} //namespace smart
