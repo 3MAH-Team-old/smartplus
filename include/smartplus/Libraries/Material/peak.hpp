57cbb62085eadcba7225fd1968f412c030c2512b
chemisky <yves.chemisky@gmail.com>
2016-05-20 22:37:24 -0400

    ODF peak class declaration

---
+++ include/smartplus/Libraries/Material/peak.hpp
@@ -0,0 +1,63 @@
+/* This file is part of SMART+.
+ 
+ SMART+ is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ SMART+ is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with SMART+.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
+
+///@file peak.hpp
+///@brief Characteristics of a peak
+///@version 0.9
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <armadillo>
+
+namespace smart{
+
+//======================================
+class peak
+//======================================
+{
+	private:
+
+	protected:
+
+	public :
+
+        int method;
+		double mean;
+		double s_dev;
+		double width;
+        double ampl;
+        arma::vec param; //Potential additional parameters
+        arma::vec densities; //density
+    
+		peak(); 	//default constructor
+    
+        peak(const int &, const double &, const double &, const double &, const double &, const arma::vec &, const arma::vec &);
+
+		peak(const peak&);	//Copy constructor
+        virtual ~peak();
+
+        virtual void construct(const double &, const double &, const double &, const double &, const double &, const bool &);
+        virtual double get_density(const double &, const double &);
+    
+		virtual peak& operator = (const peak&);
+    
+        friend std::ostream& operator << (std::ostream&, const peak&);
+};
+
+} //namespace smart
