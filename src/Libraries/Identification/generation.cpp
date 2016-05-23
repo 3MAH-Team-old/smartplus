7a48a82c2970a96db9ccc987545fa35d063bbec0
chemisky <yves.chemisky@gmail.com>
2016-05-23 14:31:13 -0400

    change in the doc

--- src/Libraries/Identification/generation.cpp
+++ src/Libraries/Identification/generation.cpp
@@ -1,12 +1,18 @@
-/* This file is part of SMART+ private.
+/* This file is part of SMART+.
  
- Only part of SMART+ is free software: you can redistribute it and/or modify
+ SMART+ is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
  
- This file is not be distributed under the terms of the GNU GPL 3.
- It is a proprietary file, copyrighted by the authors
+ SMART+ is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with SMART+.  If not, see <http://www.gnu.org/licenses/>.
+ 
  */
 
 ///@file generation.cpp
