fab7a4a88f9f5302a57c6cd3aed4237fd7d0ef47
chemisky <yves.chemisky@gmail.com>
2016-06-28 17:17:34 -0400

    changed read ODFs with data path and file name

--- src/Libraries/Material/read.cpp
+++ src/Libraries/Material/read.cpp
@@ -26,18 +26,19 @@
 #include <smartplus/parameter.hpp>
 #include <smartplus/Libraries/Material/ODF.hpp>
 
+using namespace std;
 using namespace arma;
 
 namespace smart{
 
-void read_peak(ODF &odf_rve, const int &filenumber) {
+void read_peak(ODF &odf_rve, const string &path_data, const string &inputfile) {
     
     unsigned int npeaks = 0;
     std::string buffer;
-    std::string filename = "data/Npeaks" + std::to_string(filenumber) + ".dat";
+    std::string path_inputfile = path_data + "/" + inputfile;
     std::ifstream paramodf;
     
-    paramodf.open(filename, ios::in);
+    paramodf.open(path_inputfile, ios::in);
     if(paramodf) {
         while (!paramodf.eof())
         {
@@ -48,43 +49,39 @@ void read_peak(ODF &odf_rve, const int &filenumber) {
         }
     }
     else {
-        cout << "Error: cannot open the file " << filename << " that details the peak characteristics\n";
-        exit(0);
+        cout << "Error: cannot open the file " << inputfile << " that details the peak characteristics in the folder: " << path_data << endl;
+        return;
     }
     paramodf.close();
     npeaks--;
-    
-    cout << "npeaks = " << npeaks << endl;
     
     //Generate the sub_phase vector and har-create the objects pointed buy the shared_ptrs
     odf_rve.construct(npeaks);
     
     int nparams = 0;
     
-    paramodf.open(filename, ios::in);
+    paramodf.open(path_inputfile, ios::in);
     paramodf >> buffer >> buffer >> buffer >> buffer >> buffer >> buffer >> buffer >> buffer;
     
-    for(int i=0; i<npeaks; i++) {
+    for(unsigned int i=0; i<npeaks; i++) {
         
         paramodf >> buffer >> buffer >> buffer >> buffer >> buffer >> buffer >> nparams;
         
-        cout << "nparams = " << nparams << endl;
-        
         odf_rve.peaks[i].params = zeros(nparams);
-        for(int j=0; j<odf_rve.peaks[i].params.n_elem; j++) {
+        for(unsigned int j=0; j<odf_rve.peaks[i].params.n_elem; j++) {
             paramodf >> buffer;
         }
     }
     paramodf.close();
     
-    paramodf.open(filename, ios::in);
+    paramodf.open(path_inputfile, ios::in);
     paramodf >> buffer >> buffer >> buffer >> buffer >> buffer >> buffer >> buffer >> buffer;
     
-    for(int i=0; i<npeaks; i++) {
+    for(unsigned int i=0; i<npeaks; i++) {
         
         paramodf >> odf_rve.peaks[i].number >> odf_rve.peaks[i].method >> odf_rve.peaks[i].mean >> odf_rve.peaks[i].s_dev >> odf_rve.peaks[i].width >> odf_rve.peaks[i].ampl >> buffer;
         
-        for(int j=0; j<odf_rve.peaks[i].params.n_elem; j++) {
+        for(unsigned int j=0; j<odf_rve.peaks[i].params.n_elem; j++) {
             paramodf >> odf_rve.peaks[i].params(j);
         }
         
