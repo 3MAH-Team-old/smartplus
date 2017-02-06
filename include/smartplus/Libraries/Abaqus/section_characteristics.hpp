3419529982a8717c34396a65fa83b5fff7efddb8
chemisky <yves.chemisky@gmail.com>
2017-02-06 14:35:47 +0100

    Added section Abaqus object

---
+++ include/smartplus/Libraries/Abaqus/section_characteristics.hpp
@@ -0,0 +1,63 @@
+/* This file is part of SMART+.
+ 
+ SMART+ is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ SMART+ is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with SMART+.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
+
+///@file phase_characteristics.hpp
+///@brief Characteristics of a phase, which hereditates from:
+///-material characteristics
+///@version 1.0
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <armadillo>
+#include "../Phase/phase_characteristics.hpp"
+#include "materials.hpp"
+
+namespace smart{
+
+//======================================
+class section_characteristics
+//======================================
+{
+	private:
+
+	protected:
+
+	public :
+
+        std::string elset_name;
+        int id;
+    
+        aba_material abamat;
+        std::vector<section_characteristics> sub_sections;
+        std::string sub_sections_file;
+    
+		section_characteristics(); 	//default constructor
+    
+        section_characteristics(const std::string &, const int &, const aba_material &, const std::string &);
+    
+		section_characteristics(const section_characteristics&);	//Copy constructor
+        virtual ~section_characteristics();
+    
+        virtual void update(const std::string &, const int &, const phase_characteristics &);
+
+		virtual section_characteristics& operator = (const section_characteristics&);
+        friend std::ostream& operator << (std::ostream&, const section_characteristics&);
+};
+
+} //namespace smart
