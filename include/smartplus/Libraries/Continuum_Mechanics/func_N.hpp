676acd27009d7c06edf703501689dbcd7b9e37f8
chemisky <yves.chemisky@gmail.com>
2017-03-27 17:25:50 +0200

    Added func N

---
+++ include/smartplus/Libraries/Continuum_Mechanics/func_N.hpp
@@ -0,0 +1,23 @@
+/* This file is part of SMART+ private.
+ 
+ Only part of SMART+ is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This file is not be distributed under the terms of the GNU GPL 3.
+ It is a proprietary file, copyrighted by the authors
+ */
+
+///@file func_N.hpp
+///@brief Functions that computes cumulative  laws
+///@version 1.0
+
+#include <armadillo>
+#include <string>
+
+namespace smart{
+
+void func_N(const arma::vec &, const arma::vec &, const std::string&, const std::string&, const std::string&, const std::string&);
+
+} //namespace smart
