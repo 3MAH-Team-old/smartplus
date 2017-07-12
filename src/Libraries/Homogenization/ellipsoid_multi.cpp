45cf40bfd522f8d1e303a24b8b1cd378c5173291
chemisky <yves.chemisky@gmail.com>
2017-07-12 10:18:57 +0200

    Added A_in to ellipsoid_multi

--- src/Libraries/Homogenization/ellipsoid_multi.cpp
+++ src/Libraries/Homogenization/ellipsoid_multi.cpp
@@ -53,7 +53,7 @@ vec ellipsoid_multi::wy;
 */
     
 //-------------------------------------------------------------
-ellipsoid_multi::ellipsoid_multi() : phase_multi(), S_loc(6,6), P_loc(6,6), T_loc(6,6), T(6,6)
+ellipsoid_multi::ellipsoid_multi() : phase_multi(), S_loc(6,6), P_loc(6,6), T_loc(6,6), T(6,6), T_in_loc(6,6), T_in(6,6)
 //-------------------------------------------------------------
 {
     //This calls only the constructor of the two matrix A & B
@@ -65,13 +65,15 @@ ellipsoid_multi::ellipsoid_multi() : phase_multi(), S_loc(6,6), P_loc(6,6), T_lo
 */
 
 //-------------------------------------------------------------
-ellipsoid_multi::ellipsoid_multi(const mat &mA, const mat &mA_start, const mat &mB, const mat &mB_start, const mat &mS_loc, const mat &mP_loc, const mat &mT_loc, const mat &mT) : phase_multi(mA, mA_start, mB, mB_start), S_loc(6,6), P_loc(6,6), T_loc(6,6), T(6,6)
+ellipsoid_multi::ellipsoid_multi(const mat &mA, const mat &mA_start, const mat &mB, const mat &mB_start, const vec &mA_in, const mat &mS_loc, const mat &mP_loc, const mat &mT_loc, const mat &mT, const mat &mT_in_loc, const mat &mT_in) : phase_multi(mA, mA_start, mB, mB_start, mA_in), S_loc(6,6), P_loc(6,6), T_loc(6,6), T(6,6), T_in_loc(6,6), T_in(6,6)
 //-------------------------------------------------------------
 {
     S_loc = mS_loc;
     P_loc = mP_loc;
     T_loc = mT_loc;
     T = mT;
+    T_in_loc = mT_in_loc;
+    T_in = mT_in;
 }
 
 /*!
@@ -80,13 +82,15 @@ ellipsoid_multi::ellipsoid_multi(const mat &mA, const mat &mA_start, const mat &
 */
     
 //------------------------------------------------------
-ellipsoid_multi::ellipsoid_multi(const ellipsoid_multi& pc) : phase_multi(pc), S_loc(6,6), P_loc(6,6), T_loc(6,6), T(6,6)
+ellipsoid_multi::ellipsoid_multi(const ellipsoid_multi& pc) : phase_multi(pc), S_loc(6,6), P_loc(6,6), T_loc(6,6), T(6,6), T_in_loc(6,6), T_in(6,6)
 //------------------------------------------------------
 {
     S_loc = pc.S_loc;
     P_loc = pc.P_loc;
     T_loc = pc.T_loc;
     T = pc.T;
+    T_in_loc = pc.T_in_loc;
+    T_in = pc.T_in;
 }
 
 /*!
@@ -136,7 +140,7 @@ void ellipsoid_multi::fillT(const mat& Lt_m, const mat& Lt, const ellipsoid &ell
 
 //-------------------------------------
 void ellipsoid_multi::fillT_iso(const mat& Lt_m, const mat& Lt, const ellipsoid &ell)
-//This method corresponf to the Ponte-astenada and Willis method
+//This method corresponf to the isotropization method
 //-------------------------------------
 {
     mat Lt_m_iso = Isotropize(Lt_m);
@@ -147,7 +151,24 @@ void ellipsoid_multi::fillT_iso(const mat& Lt_m, const mat& Lt, const ellipsoid 
     
     T = rotate_l2g_A(T_loc, ell.psi_geom, ell.theta_geom, ell.phi_geom);
 }
-        
+
+//-------------------------------------
+void ellipsoid_multi::fillT_mec_in(const mat& L_m, const mat& L, const ellipsoid &ell)
+//This method corresponds to the Eshelby inhomogeneous inhomogeneity - inelasticity problem. It calculates
+//the interaction tensors T for the elastic and the inelastic part.
+//-------------------------------------
+{
+    mat L_m_local_geom = rotate_g2l_L(L_m, ell.psi_geom, ell.theta_geom, ell.phi_geom);
+    S_loc = Eshelby(L_m_local_geom, ell.a1, ell.a2, ell.a3, x, wx, y, wy, mp, np);
+    mat L_local_geom = rotate_g2l_L(L, ell.psi_geom, ell.theta_geom, ell.phi_geom);
+    
+    T_loc = inv(eye(6,6) + S_loc*inv(L_m_local_geom)*(L_local_geom - L_m_local_geom));
+    T = rotate_l2g_A(T_loc, ell.psi_geom, ell.theta_geom, ell.phi_geom);
+    
+    T_in_loc = (eye(6,6)-T_loc)*inv(L_m_local_geom - L_local_geom);
+    T_in = rotate_l2g_M(T_in_loc, ell.psi_geom, ell.theta_geom, ell.phi_geom);
+}
+    
 //----------------------------------------------------------------------
 ellipsoid_multi& ellipsoid_multi::operator = (const ellipsoid_multi& pc)
 //----------------------------------------------------------------------
@@ -162,6 +183,8 @@ ellipsoid_multi& ellipsoid_multi::operator = (const ellipsoid_multi& pc)
     P_loc = pc.P_loc;
     T_loc = pc.T_loc;
     T = pc.T;
+    T_in_loc = pc.T_in_loc;
+    T_in = pc.T_in;
     
 	return *this;
 }
@@ -180,10 +203,14 @@ ostream& operator << (ostream& s, const ellipsoid_multi& pc)
     s << pc.S_loc;
     s << "Display Polarization tensor (local coordinates):\n";
     s << pc.P_loc;
-    s << "Display Interaction concentration tensor (global coordinates):\n";
+    s << "Display Interaction concentration tensor (local coordinates):\n";
     s << pc.T_loc;
     s << "Display Interaction concentration tensor (global coordinates):\n";
     s << pc.T;
+    s << "Display Inelastic Interaction concentration tensor (local coordinates):\n";
+    s << pc.T_in_loc;
+    s << "Display Inelastic Interaction concentration tensor (global coordinates):\n";
+    s << pc.T_in;
     
     s << "\n\n";
 
