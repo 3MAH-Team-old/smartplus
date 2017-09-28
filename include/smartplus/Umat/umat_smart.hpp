a7b7ba4f42da6a88fe93d147f123e1d8dcd032ae
chemisky <yves.chemisky@gmail.com>
2017-09-28 14:54:51 +0200

    Added solver_type to umat selection

--- include/smartplus/Umat/umat_smart.hpp
+++ include/smartplus/Umat/umat_smart.hpp
@@ -53,14 +53,13 @@ void abaqus2smart_T(double *, double *, double *, double *, double &, const doub
     
 void select_umat_T(phase_characteristics &, const arma::mat &, const double &, const double &, const int &, const int &, const bool &, double &);
     
-void select_umat_M(phase_characteristics &, const arma::mat &, const double &, const double &, const int &, const int &, const bool &, double &);
-
+void select_umat_M(phase_characteristics &, const arma::mat &, const double &, const double &, const int &, const int &, const bool &, const int &, double &);
+    
 void run_umat_T(phase_characteristics &, const arma::mat &, const double &, const double &, const int &, const int &, bool &, double &);
 
-void run_umat_M(phase_characteristics &, const arma::mat &, const double &, const double &, const int &, const int &, bool &, double &);
-
+void run_umat_M(phase_characteristics &, const arma::mat &, const double &, const double &, const int &, const int &, bool &, const int &, double &);
+    
 void smart2abaqus_M(double *, double *, double *, const int &, const int &, const arma::vec &, const arma::vec &, const arma::vec &, const arma::mat &);
-
+    
 void smart2abaqus_T(double *, double *, double *, double *, double &, double &, double *, const int &, const int &, const arma::vec &, const arma::vec &, const double &, const arma::vec &, const arma::vec &, const arma::mat &, const arma::mat &, const arma::mat &, const arma::mat &);
-        
 } //namespace smart
