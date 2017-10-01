d8dd09339947d1df13dd2b2c9f89d3e08538b09b
chemisky <yves.chemisky@gmail.com>
2017-10-01 23:00:41 +0200

    Updated umat_smart with new solver and a few new thermomechanical laws

--- src/Umat/umat_smart.cpp
+++ src/Umat/umat_smart.cpp
@@ -12,7 +12,6 @@
  
  You should have received a copy of the GNU General Public License
  along with SMART+.  If not, see <http://www.gnu.org/licenses/>.
- 
  */
 
 ///@file umat_smart.cpp
@@ -40,8 +39,11 @@
 #include <smartplus/Umat/Mechanical/Damage/damage_LLD_0.hpp>
 
 #include <smartplus/Umat/Thermomechanical/Elasticity/elastic_isotropic.hpp>
+#include <smartplus/Umat/Thermomechanical/Elasticity/elastic_transverse_isotropic.hpp>
+#include <smartplus/Umat/Thermomechanical/Elasticity/elastic_orthotropic.hpp>
 #include <smartplus/Umat/Thermomechanical/Plasticity/plastic_isotropic_ccp.hpp>
 #include <smartplus/Umat/Thermomechanical/Plasticity/plastic_kin_iso_ccp.hpp>
+
 #include <smartplus/Umat/Thermomechanical/SMA/unified_T.hpp>
 
 #include <smartplus/Libraries/Phase/material_characteristics.hpp>
@@ -340,10 +342,10 @@ void abaqus2smart_T(double *stress, double *ddsdde, double *ddsddt, double *drpl
     
 }
     
-void select_umat_T(phase_characteristics &rve, const mat &DR,const double &Time,const double &DTime, const int &ndi, const int &nshr, const bool &start, double &tnew_dt)
+void select_umat_T(phase_characteristics &rve, const mat &DR,const double &Time,const double &DTime, const int &ndi, const int &nshr, const bool &start, const int &solver_type, double &tnew_dt)
 {
     std::map<string, int> list_umat;
-    list_umat = {{"ELISO",1},{"ELIST",2},{"ELORT",3},{"EPICP",4},{"EPKCP",5},{"SMAUT",6}};
+    list_umat = {{"ELISO",1},{"ELIST",2},{"ELORT",3},{"EPICP",4},{"EPKCP",5},{"SMAUT",9}};
 
     rve.global2local();
     auto umat_T = std::dynamic_pointer_cast<state_variables_T>(rve.sptr_sv_local);
@@ -353,20 +355,26 @@ void select_umat_T(phase_characteristics &rve, const mat &DR,const double &Time,
             umat_elasticity_iso_T(umat_T->Etot, umat_T->DEtot, umat_T->sigma, umat_T->r, umat_T->dSdE, umat_T->dSdT, umat_T->drdE, umat_T->drdT, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_T->nstatev, umat_T->statev, umat_T->T, umat_T->DT, Time, DTime, umat_T->Wm(0), umat_T->Wm(1), umat_T->Wm(2), umat_T->Wm(3), umat_T->Wt(0), umat_T->Wt(1), umat_T->Wt(2), ndi, nshr, start, tnew_dt);
             break;
         }
+        case 2: {
+            umat_elasticity_trans_iso_T(umat_T->Etot, umat_T->DEtot, umat_T->sigma, umat_T->r, umat_T->dSdE, umat_T->dSdT, umat_T->drdE, umat_T->drdT, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_T->nstatev, umat_T->statev, umat_T->T, umat_T->DT, Time, DTime, umat_T->Wm(0), umat_T->Wm(1), umat_T->Wm(2), umat_T->Wm(3), umat_T->Wt(0), umat_T->Wt(1), umat_T->Wt(2), ndi, nshr, start, tnew_dt);
+            break;
+        }
+        case 3: {
+            umat_elasticity_ortho_T(umat_T->Etot, umat_T->DEtot, umat_T->sigma, umat_T->r, umat_T->dSdE, umat_T->dSdT, umat_T->drdE, umat_T->drdT, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_T->nstatev, umat_T->statev, umat_T->T, umat_T->DT, Time, DTime, umat_T->Wm(0), umat_T->Wm(1), umat_T->Wm(2), umat_T->Wm(3), umat_T->Wt(0), umat_T->Wt(1), umat_T->Wt(2), ndi, nshr, start, tnew_dt);
+            break;
+        }
         case 4: {
             umat_plasticity_iso_CCP_T(umat_T->Etot, umat_T->DEtot, umat_T->sigma, umat_T->r, umat_T->dSdE, umat_T->dSdT, umat_T->drdE, umat_T->drdT, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_T->nstatev, umat_T->statev, umat_T->T, umat_T->DT, Time, DTime, umat_T->Wm(0), umat_T->Wm(1), umat_T->Wm(2), umat_T->Wm(3), umat_T->Wt(0), umat_T->Wt(1), umat_T->Wt(2), ndi, nshr, start, tnew_dt);
             break;
         }
         case 5: {
             umat_plasticity_kin_iso_CCP_T(umat_T->Etot, umat_T->DEtot, umat_T->sigma, umat_T->r, umat_T->dSdE, umat_T->dSdT, umat_T->drdE, umat_T->drdT, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_T->nstatev, umat_T->statev, umat_T->T, umat_T->DT, Time, DTime, umat_T->Wm(0), umat_T->Wm(1), umat_T->Wm(2), umat_T->Wm(3), umat_T->Wt(0), umat_T->Wt(1), umat_T->Wt(2), ndi, nshr, start, tnew_dt);
-            break;
+           break;
         }
-        case 6: {
+        case 9: {
             umat_sma_unified_T_T(umat_T->Etot, umat_T->DEtot, umat_T->sigma, umat_T->r, umat_T->dSdE, umat_T->dSdT, umat_T->drdE, umat_T->drdT, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_T->nstatev, umat_T->statev, umat_T->T, umat_T->DT, Time, DTime, umat_T->Wm(0), umat_T->Wm(1), umat_T->Wm(2), umat_T->Wm(3), umat_T->Wt(0), umat_T->Wt(1), umat_T->Wt(2), ndi, nshr, start, tnew_dt);
             break;
         }
-            
-            
         default: {
             cout << "Error: The choice of Thermomechanical Umat could not be found in the umat library :" << rve.sptr_matprops->umat_name << "\n";
                 exit(0);
@@ -376,11 +384,11 @@ void select_umat_T(phase_characteristics &rve, const mat &DR,const double &Time,
     
 }
     
-void select_umat_M(phase_characteristics &rve, const mat &DR,const double &Time,const double &DTime, const int &ndi, const int &nshr, const bool &start, double &tnew_dt)
+void select_umat_M(phase_characteristics &rve, const mat &DR,const double &Time,const double &DTime, const int &ndi, const int &nshr, const bool &start, const int &solver_type, double &tnew_dt)
 {
 	
     std::map<string, int> list_umat;
-    list_umat = {{"ELISO",1},{"ELIST",2},{"ELORT",3},{"EPICP",4},{"EPKCP",5},{"SMAUT",6},{"LLDM0",7},{"MIHEN",100},{"MIMTN",101},{"MISCN",102},{"MIPCW",103},{"MIPLN",104}};
+    list_umat = {{"ELISO",1},{"ELIST",2},{"ELORT",3},{"EPICP",4},{"EPKCP",5},{"SMAUT",6},{"LLDM0",8},{"MIHEN",100},{"MIMTN",101},{"MISCN",103},{"MIPLN",104}};
     
         rve.global2local();
         auto umat_M = std::dynamic_pointer_cast<state_variables_M>(rve.sptr_sv_local);
@@ -389,33 +397,34 @@ void select_umat_M(phase_characteristics &rve, const mat &DR,const double &Time,
                 
             case 1: {
                 umat_elasticity_iso(umat_M->Etot, umat_M->DEtot, umat_M->sigma, umat_M->Lt, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_M->nstatev, umat_M->statev, umat_M->T, umat_M->DT, Time, DTime, umat_M->Wm(0), umat_M->Wm(1), umat_M->Wm(2), umat_M->Wm(3), ndi, nshr, start, tnew_dt);
-                break;
-            }
-            case 2: {
+                 break;
+             }
+             case 2: {
                 umat_elasticity_trans_iso(umat_M->Etot, umat_M->DEtot, umat_M->sigma, umat_M->Lt, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_M->nstatev, umat_M->statev, umat_M->T, umat_M->DT, Time, DTime, umat_M->Wm(0), umat_M->Wm(1), umat_M->Wm(2), umat_M->Wm(3), ndi, nshr, start, tnew_dt);
-                break;
-            }
-            case 3: {
+                 break;
+             }
+             case 3: {
                 umat_elasticity_ortho(umat_M->Etot, umat_M->DEtot, umat_M->sigma, umat_M->Lt, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_M->nstatev, umat_M->statev, umat_M->T, umat_M->DT, Time, DTime, umat_M->Wm(0), umat_M->Wm(1), umat_M->Wm(2), umat_M->Wm(3), ndi, nshr, start, tnew_dt);
-                break;
-            }
-            case 4: {
-                umat_plasticity_iso_CCP(umat_M->Etot, umat_M->DEtot, umat_M->sigma, umat_M->Lt, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_M->nstatev, umat_M->statev, umat_M->T, umat_M->DT, Time, DTime, umat_M->Wm(0), umat_M->Wm(1), umat_M->Wm(2), umat_M->Wm(3), ndi, nshr, start, tnew_dt);
-                break;
-            }
-            case 5: {
+                 break;
+             }
+             case 4: {
+                 //with RNL
+                umat_plasticity_iso_CCP(umat_M->Etot, umat_M->DEtot, umat_M->sigma, umat_M->Lt, umat_M->L, umat_M->sigma_in, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_M->nstatev, umat_M->statev, umat_M->T, umat_M->DT, Time, DTime, umat_M->Wm(0), umat_M->Wm(1), umat_M->Wm(2), umat_M->Wm(3), ndi, nshr, start, solver_type, tnew_dt);
+                 break;
+             }
+             case 5: {
                 umat_plasticity_kin_iso_CCP(umat_M->Etot, umat_M->DEtot, umat_M->sigma, umat_M->Lt, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_M->nstatev, umat_M->statev, umat_M->T, umat_M->DT, Time, DTime, umat_M->Wm(0), umat_M->Wm(1), umat_M->Wm(2), umat_M->Wm(3), ndi, nshr, start, tnew_dt);
-                break;
-            }
+                 break;
+             }
             case 6: {
                 umat_sma_unified_T(umat_M->Etot, umat_M->DEtot, umat_M->sigma, umat_M->Lt, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_M->nstatev, umat_M->statev, umat_M->T, umat_M->DT, Time, DTime, umat_M->Wm(0), umat_M->Wm(1), umat_M->Wm(2), umat_M->Wm(3), ndi, nshr, start, tnew_dt);
                 break;
             }
-            case 7: {
+            case 8: {
                 umat_damage_LLD_0(umat_M->Etot, umat_M->DEtot, umat_M->sigma, umat_M->Lt, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_M->nstatev, umat_M->statev, umat_M->T, umat_M->DT, Time, DTime, umat_M->Wm(0), umat_M->Wm(1), umat_M->Wm(2), umat_M->Wm(3), ndi, nshr, start, tnew_dt);
                 break;
             }
-            case 100: case 101: case 102: case 103: case 104: {
+            case 100: case 101: case 103: case 104: {
                 umat_multi(rve, DR, Time, DTime, ndi, nshr, start, tnew_dt, list_umat[rve.sptr_matprops->umat_name]);
                 break;
             }
@@ -427,31 +436,31 @@ void select_umat_M(phase_characteristics &rve, const mat &DR,const double &Time,
         rve.local2global();
 
 }
-
-void run_umat_T(phase_characteristics &rve, const mat &DR,const double &Time,const double &DTime, const int &ndi, const int &nshr, bool &start, double &tnew_dt)
+    
+void run_umat_T(phase_characteristics &rve, const mat &DR,const double &Time,const double &DTime, const int &ndi, const int &nshr, bool &start, const int &solver_type, double &tnew_dt)
 {
     
     tnew_dt = 1.;
     
-    select_umat_T(rve, DR, Time, DTime, ndi, nshr, start, tnew_dt);
+    select_umat_T(rve, DR, Time, DTime, ndi, nshr, start, solver_type, tnew_dt);
     
     if (Time + DTime > limit) {
         start = false;
     }
 }
 
-void run_umat_M(phase_characteristics &rve, const mat &DR,const double &Time,const double &DTime, const int &ndi, const int &nshr, bool &start, double &tnew_dt)
+void run_umat_M(phase_characteristics &rve, const mat &DR,const double &Time,const double &DTime, const int &ndi, const int &nshr, bool &start, const int &solver_type, double &tnew_dt)
 {
     
     tnew_dt = 1.;
     
-    select_umat_M(rve, DR, Time, DTime, ndi, nshr, start, tnew_dt);
+    select_umat_M(rve, DR, Time, DTime, ndi, nshr, start, solver_type, tnew_dt);
     
     if (Time + DTime > limit) {
         start = false;
     }
-}	
-
+}
+    
 void smart2abaqus_M(double *stress, double *ddsdde, double *statev, const int &ndi, const int &nshr, const vec &sigma, const vec &statev_smart, const vec &Wm, const mat &Lt)
 {
     
@@ -696,5 +705,5 @@ void smart2abaqus_T(double *stress, double *ddsdde, double *ddsddt, double *drpl
         statev[i+7] = statev_smart(i);
     }
 }
-	
+    
 } //namespace smart
